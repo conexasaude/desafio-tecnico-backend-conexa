6e820f9204688b0410de662701c414215803a555
CleuJunior <cleu.junior@gmail.com>
2023-12-17 10:47:31 -0300

    Initial Commit

---
+++ src/main/java/br/com/cleonildo/schedulingappoinment/SchedulingappoinmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo.schedulingappoinment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingappoinmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingappoinmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cleonildo/schedulingappoinment/SchedulingappoinmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo.schedulingappoinment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingappoinmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
