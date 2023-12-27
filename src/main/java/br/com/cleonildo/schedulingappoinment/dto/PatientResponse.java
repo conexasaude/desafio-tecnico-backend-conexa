090d23646e072bb049072228605074e6a6eee59f
CleuJunior <cleu.junior@gmail.com>
2023-12-27 09:18:38 -0300

    refactor: Changed to record.

--- src/main/java/br/com/cleonildo/schedulingappoinment/dto/PatientResponse.java
+++ src/main/java/br/com/cleonildo/schedulingappoinment/dto/PatientResponse.java
@@ -2,18 +2,11 @@ package br.com.cleonildo.schedulingappoinment.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @JsonPropertyOrder({"id", "nome", "cpf"})
-public class PatientResponse {
-    private Long id;
-    @JsonProperty("nome")
-    private String name;
-    private String cpf;
-
-}
+public record PatientResponse(
+        Long id,
+        @JsonProperty("nome")
+        String name,
+        String cpf
+) { }
